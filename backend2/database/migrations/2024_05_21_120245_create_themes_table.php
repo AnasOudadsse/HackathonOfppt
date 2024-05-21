b2a034385a653e1ad8bc47e6be836b966979709a
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-05-21 14:09:01 +0100

    "Updated create_themes_table migration: swapped use statements, added 4 columns to themes table"

--- backend2/database/migrations/2024_05_21_120245_create_themes_table.php
+++ backend2/database/migrations/2024_05_21_120245_create_themes_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('domaines_id')->constrained('domaines')->onDelete('cascade');
+            $table->string('intitule_theme', 255);
+            $table->integer('duree_formation');
+            $table->string('status');
             $table->timestamps();
         });
     }
