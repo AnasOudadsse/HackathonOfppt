26cc43ec6456be504c31e47caff12ab5584baba2
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-05-21 13:34:20 +0100

    "Deleted create_anas_table migration and modified create_roles_table migration to add role_name column"

--- backend2/database/migrations/2024_05_21_120048_create_roles_table.php
+++ backend2/database/migrations/2024_05_21_120048_create_roles_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+            Schema::create('roles', function (Blueprint $table) {
+                $table->id('role_id');
+                $table->string('role_name');
+                $table->timestamps();
+            });
     }
 
     /**
