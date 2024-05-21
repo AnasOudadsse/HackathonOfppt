e29d0f98c9bd4c48f13f1772c2fd7380e534df56
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-05-21 14:39:20 +0100

    "Deleted AnasController.php, updated namespace imports in two migration files, and added columns to certifications and diplomes tables."

--- backend2/database/migrations/2024_05_21_120338_create_diplomes_table.php
+++ backend2/database/migrations/2024_05_21_120338_create_diplomes_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('diplomes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('domaines_id')->constrained('domaines')->onDelete('cascade');
+            $table->string('intitule_diplome', 45);
+            $table->string('organisme_diplome', 45);
+            $table->string('type_diplome', 45);
             $table->timestamps();
         });
     }
